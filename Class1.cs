c58eeaf567e73fdd44e85c74bb9a375101c43431
haritha <harithatkumar@gmail.com>
2019-09-24 21:54:26 -0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oops
+{
+    class Class1
+    {
+        
+
+        public long contact;
+        public int myfriendage;
+        public string name;
+        
+    }
+
+    
+
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Oops
+{
+    class Program: Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Class1 cs = new Class1();
+            
+            
+            cs.myfriendage = 25;
+            cs.name = "haritha";
+            cs.contact = 9440430078;
+           
+            Console.WriteLine("my friends name {0} ",cs.name);
+            Console.WriteLine("my friend age {0}" , cs.myfriendage);
+            Console.WriteLine("my friend contact {0}" , cs.contact);
+
+
+        }
+    }
+}
